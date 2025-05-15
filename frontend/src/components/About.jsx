e95286b0ce5e78908ebcf1d75555b8b8cf673f56
Md Shoyeb <mdshoyeb241@gmail.com>
2025-05-15 23:35:51 +0530

    first-commit

---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { HiOutlineArrowRight } from "react-icons/hi";
+
+const About = () => {
+  return (
+    <>
+      <section className="about" id="about">
+        <div className="container">
+          <div className="banner">
+            <div className="top">
+              <h1 className="heading">ABOUT US</h1>
+              <p>The only thing we're serious about is food.</p>
+            </div>
+            <p className="mid">
+              Welcome to Hichki Restaurant, where tradition meets taste. Located in the heart of the city, Hichki is more than just a place to dine — it's an experience that blends the rich flavors of Indian cuisine with a modern twist.
+            </p>
+            <Link to={"/"}>
+              Explore Menu{" "}
+              <span>
+                <HiOutlineArrowRight />
+              </span>
+            </Link>
+          </div>
+          <div className="banner">
+            <img src="about.png" alt="about" />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default About;
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">MD SHOYEB</div>
+          <div className="right">
+            <p>TOWN THANA ROAD,HAJIPUR</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>📞(+91) 6203220275</p>
+          </div>
+          <div className="right">
+            <p>© 2025 All Rights Reserved</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
